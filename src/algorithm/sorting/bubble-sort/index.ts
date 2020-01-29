597986af6dbe4feea2e558435cbc18849156c2d3
KangHyun Lee <rkdgusdl92+git@gmail.com>
2020-01-29 19:01:42 +0900

    Add Comment

--- src/algorithm/sorting/bubble-sort/index.ts
+++ src/algorithm/sorting/bubble-sort/index.ts
@@ -1,8 +1,13 @@
 console.time('BubbleSort');
 export const BubbleSort = (arr: number[] = []): number[] => {
   const len = arr.length;
+  // swapped 플래그 생성
   let swapped = false;
 
+  /*
+   * swapped 이 일어나지 않을 때 까지 계속 실행시킨다.
+   * 한 번 이상 돌아야 하기 때문에 do ... while 로 작성한다.
+   */
   do {
     swapped = false;
 
